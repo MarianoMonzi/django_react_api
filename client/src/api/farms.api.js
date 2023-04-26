f4438c55aad0d2d083bb819810d6a60f44506dde
MarianoMonzi <arielmariano.monzon2002@gmail.com>
2023-04-26 13:40:41 -0300

    edited settings

--- client/src/api/farms.api.js
+++ client/src/api/farms.api.js
@@ -1,15 +1,17 @@
 import axios from 'axios'
 
+const URL = process.env.NODE_ENV === "production" ? import.meta.env.VITE_BACKEND_URL: 'http://localhost:8000';
+
 const farmApi = axios.create({
-    baseURL: 'http://localhost:8000/farms/api/v1/farms'
-})
+    baseURL: `${URL}/farms/api/v1/farms`,
+});
 
-export const getAllFarms = () => farmApi.get('/')
+export const getAllFarms = () => farmApi.get('/');
 
-export const getFarm = (id) => farmApi.get(`/${id}/`)
+export const getFarm = (id) => farmApi.get(`/${id}/`);
 
-export const createFarm = (farm) => farmApi.post('/', farm)
+export const createFarm = (farm) => farmApi.post('/', farm);
 
-export const deleteFarm = (id) => farmApi.delete(`/${id}`)
+export const deleteFarm = (id) => farmApi.delete(`/${id}`);
 
-export const updateFarm = (id, farm) => farmApi.put(`/${id}/`, farm)
+export const updateFarm = (id, farm) => farmApi.put(`/${id}/`, farm);
